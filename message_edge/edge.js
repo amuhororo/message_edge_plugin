f8edf860c2ba837d067190371b1c80c44498fd99
amuhororo <ruru.amu@gmail.com>
2022-05-21 21:20:50 +0900

    v3.02 コード修正。機能変更なし。

--- message_edge/edge.js
+++ message_edge/edge.js
@@ -1,95 +1,94 @@
 //【メッセージ縁取りプラグイン】
-// Ver.3.01 2022/5/15
+// Ver.3.02 2022/5/22
 // by hororo https://memocho.no-tenki.me/
 
-if(typeof memocho === 'undefined') {
-	var memocho = {};
-	memocho.tag = {};
-};
+(function(){
+	
+	const message_edge = {
+		pm : {
+			edge_flag : "true",
+			shadow_flag : "true",
+			edge_blur : "1",
+			edge_offset : "1",
+			shadow_blur : "1",
+			shadow_offset : "1",
+			clear : "false",
+			vertical : "false"
+		},
+		start : function(pm) {
+			//初期値設定 *flagはビルダー用
+			if(pm.edge_flag == "false") pm.edge = "";
+			if(pm.shadow_flag == "false") pm.shadow = "";
+			let edge = "";
+			let shadow = "";
+			let layer;
 
-memocho.tag.message_edge = {
-	pm : {
-		edge_flag : "true",
-		shadow_flag : "true",
-		edge_blur : "1",
-		edge_offset : "1",
-		shadow_blur : "1",
-		shadow_offset : "1",
-		clear : "false",
-		vertical : "false"
-	},
-	start : function(pm) {
-		//初期値設定 *flagはビルダー用
-		if(pm.edge_flag == "false") pm.edge = "";
-		if(pm.shadow_flag == "false") pm.shadow = "";
-		if(pm.edge) pm.edge = $.convertColor(pm.edge);
-		if(pm.shadow) pm.shadow = $.convertColor(pm.shadow);
-		if (pm.edge && pm.shadow){
-			pm.shadow_offset = parseInt(pm.shadow_offset) + parseInt(pm.edge_offset); //edge併用の場合edge_offset値分ずらす
-		}
-		let edge = "";
-		let shadow = "";
-		let layer;
-		const find = {
-			"chara_name" : ".chara_name_area",
-			"message" : ".message_inner"
-		};
-
-		//縁取り
-		if(pm.edge){
-			edge = [];
-			edge[0] = pm.edge + ' ' + pm.edge_offset + 'px ' + pm.edge_offset + 'px ' + pm.edge_blur + 'px'; //上右
-			edge[1] = pm.edge + ' ' + pm.edge_offset + 'px -' + pm.edge_offset + 'px ' + pm.edge_blur + 'px'; //上左
-			edge[2] = pm.edge + ' -' + pm.edge_offset + 'px -' + pm.edge_offset + 'px ' + pm.edge_blur + 'px'; //下右
-			edge[3] = pm.edge + ' -' + pm.edge_offset + 'px ' + pm.edge_offset + 'px ' + pm.edge_blur + 'px'; //下左
-			edge[4] = pm.edge + ' ' + pm.edge_offset + 'px 0 ' + pm.edge_blur + 'px'; //上
-			edge[5] = pm.edge + ' -' + pm.edge_offset + 'px 0 ' + pm.edge_blur + 'px'; //下
-			edge[6] = pm.edge + ' 0 ' + pm.edge_offset + 'px ' + pm.edge_blur + 'px'; //右
-			edge[7] = pm.edge + ' 0 -' + pm.edge_offset + 'px ' + pm.edge_blur + 'px'; //左
-		}
-
-		//影
-		if (pm.shadow) {
-			if(pm.vertical == "true"){
-				shadow = pm.shadow + ' -' + pm.shadow_offset + 'px ' + pm.shadow_offset + 'px ' + pm.shadow_blur + 'px';
-			}else{
-				shadow = pm.shadow + ' ' + pm.shadow_offset + 'px ' + pm.shadow_offset + 'px ' + pm.shadow_blur + 'px';
+			//縁取り
+			if(pm.edge){
+				pm.edge = $.convertColor(pm.edge);
+				edge = [];
+				edge[0] = pm.edge + ' ' + pm.edge_offset + 'px ' + pm.edge_offset + 'px ' + pm.edge_blur + 'px'; //上右
+				edge[1] = pm.edge + ' ' + pm.edge_offset + 'px -' + pm.edge_offset + 'px ' + pm.edge_blur + 'px'; //上左
+				edge[2] = pm.edge + ' -' + pm.edge_offset + 'px -' + pm.edge_offset + 'px ' + pm.edge_blur + 'px'; //下右
+				edge[3] = pm.edge + ' -' + pm.edge_offset + 'px ' + pm.edge_offset + 'px ' + pm.edge_blur + 'px'; //下左
+				edge[4] = pm.edge + ' ' + pm.edge_offset + 'px 0 ' + pm.edge_blur + 'px'; //上
+				edge[5] = pm.edge + ' -' + pm.edge_offset + 'px 0 ' + pm.edge_blur + 'px'; //下
+				edge[6] = pm.edge + ' 0 ' + pm.edge_offset + 'px ' + pm.edge_blur + 'px'; //右
+				edge[7] = pm.edge + ' 0 -' + pm.edge_offset + 'px ' + pm.edge_blur + 'px'; //左
 			}
-			if(pm.edge) edge = edge + ',' + shadow;
-			else  edge = shadow;
-		}
 
-		if(pm.layer){
-			if(pm.layer.match(/message/) || $.isNumeric(pm.layer)){
-				for (let key in find) {
-					if(!pm.find || pm.find == key){
-						layer = $("."+pm.layer+"_fore").find(find[key]);
-						if(pm.clear == "true") layer.css("text-shadow","");
-						else layer.css("text-shadow",edge);
-					}
+			//影
+			if(pm.shadow) {
+				pm.shadow = $.convertColor(pm.shadow);
+				//edge併用の場合edge_offset値分ずらす
+				if(pm.edge) pm.shadow_offset = parseInt(pm.shadow_offset) + parseInt(pm.edge_offset);
+				//縦書きか
+				if(pm.vertical == "true"){
+					shadow = pm.shadow + ' -' + pm.shadow_offset + 'px ' + pm.shadow_offset + 'px ' + pm.shadow_blur + 'px';
+				}else{
+					shadow = pm.shadow + ' ' + pm.shadow_offset + 'px ' + pm.shadow_offset + 'px ' + pm.shadow_blur + 'px';
+				}
+				edge.push(shadow);
+			}
+
+			//クリア
+			if(pm.clear == "true") edge = "";
+
+			//スタイル指定
+			if(pm.layer){
+				layer = pm.layer.match(/message|[0-9]/) ? $("."+pm.layer+"_fore") : $("."+pm.layer);
+				if(pm.layer.match(/message/)){
+					message_style();
+				} else {
+					if(pm.find) layer = layer.find("."+pm.find);
+					layer.css("text-shadow",edge);
 				}
 			}else{
-				layer = $("."+pm.layer);
-				if(pm.find) layer = layer.find("."+pm.find);
-				if(pm.clear == "true") layer.css("text-shadow","");
-				else layer.css("text-shadow",edge);
+				const num = parseInt(TYRANO.kag.config.numMessageLayers);
+				for(let i=0; i< num ; i++) {
+					layer = $(".message"+ i +"_fore");
+					message_style();
+				}
 			}
-		}else{
-			const num = parseInt(TYRANO.kag.config.numMessageLayers);
-			for (let i=0; i< num ; i++) {
-				for (let key in find) {
+
+			function message_style(){
+				const find = {
+					"chara_name" : ".chara_name_area",
+					"message" : ".message_inner"
+				};
+				for(let key in find) {
 					if(!pm.find || pm.find == key){
-						layer = $(".message"+ i +"_fore").find(find[key]);
-						if(pm.clear == "true") layer.css("text-shadow","");
-						else layer.css("text-shadow",edge);
+						layer.find(find[key]).css("text-shadow",edge);
 					}
 				}
 			}
-		}
-		TYRANO.kag.ftag.nextOrder();
-	}
-}
 
-//ティラノスクリプトにタグを定義
-TYRANO.kag.ftag.master_tag.message_edge = object(memocho.tag.message_edge);
-TYRANO.kag.ftag.master_tag.message_edge.kag = TYRANO.kag;
+			TYRANO.kag.ftag.nextOrder();
+		}
+	};
+
+	//ティラノスクリプトにタグを定義
+	TYRANO.kag.ftag.master_tag.message_edge = object(message_edge);
+	TYRANO.kag.ftag.master_tag.message_edge.kag = TYRANO.kag;
+
+}());
